ceb7faeab8eac402d074208843ae01d7451b42dc
Håvard Syslak <haavard.syslak@gmail.com>
2024-02-17 12:43:41 +0100

    add obstruction switch

--- localelevator/elevator/elevator.go
+++ localelevator/elevator/elevator.go
@@ -10,172 +10,185 @@ import (
 type ElevatorState int
 
 const (
-    IDLE ElevatorState = iota
-    MOVING
-    DOOR_OPEN
+	IDLE ElevatorState = iota
+	MOVING
+	DOOR_OPEN
 )
 
 type Order struct {
-    Type elevio.ButtonType
-    AtFloor int
+	Type    elevio.ButtonType
+	AtFloor int
 }
 
 type Requests struct {
-    Up []bool
-    Down []bool
-    ToFloor []bool
+	Up      []bool
+	Down    []bool
+	ToFloor []bool
 }
 
 type Elevator struct {
-    Dir elevio.MotorDirection
-    State ElevatorState
-    Requests Requests
-    MaxFloor int
-    MinFloor int
-    CurrentFloor int
+	Dir          elevio.MotorDirection
+	State        ElevatorState
+	Requests     Requests
+	MaxFloor     int
+	MinFloor     int
+	CurrentFloor int
 }
 
-func Init() (int) {
-    // Init elevator
-    // Run elevator to known floor
-    elevio.Init("localhost:15657", 4)
-    floor := elevio.GetFloor()
-    if floor == -1 {
-        floorSensCh := make(chan int)
-        go elevio.PollFloorSensor(floorSensCh)
-        return goToKnownFloor(floorSensCh)
-    }
-    return floor
+func Init() int {
+	// Init elevator
+	// Run elevator to known floor
+	elevio.Init("localhost:15657", 4)
+	floor := elevio.GetFloor()
+	if floor == -1 {
+		floorSensCh := make(chan int)
+		go elevio.PollFloorSensor(floorSensCh)
+		return goToKnownFloor(floorSensCh)
+	}
+	return floor
 }
 
+func goToKnownFloor(floorSenseCh chan int) int {
+	// go up for 4 sec -> reach a floor -> return floor
+	// Else go down for 4 sec -> reach a floor? -> return floor
+	// Else panic
+	ticker := time.NewTicker(time.Second * 4)
+	hasTimedOut := false
+	GoUp()
+	for {
+		select {
+		case floor := <-floorSenseCh:
+			Stop()
+			return floor
+		case <-ticker.C:
+			if !hasTimedOut {
+				hasTimedOut = true
+				GoDown()
+			} else {
+				Stop()
+				panic("Elevator is stuck!")
+			}
+		}
+	}
+}
 
-func goToKnownFloor(floorSenseCh chan int) (int) {
-    // go up for 4 sec -> reach a floor -> return floor
-    // Else go down for 4 sec -> reach a floor? -> return floor
-    // Else panic
-    ticker := time.NewTicker(time.Second * 4)
-    hasTimedOut := false
-    GoUp()
+func PollElevatorIO(buttonCh chan elevio.ButtonEvent,
+	florrSensCh chan int,
+	stopButtonCh chan bool,
+	obstructionSwitchCh chan bool) {
+	go elevio.PollButtons(buttonCh)
+	go elevio.PollFloorSensor(florrSensCh)
+	go elevio.PollStopButton(stopButtonCh)
+	go elevio.PollObstructionSwitch(obstructionSwitchCh)
+}
+
+func GoUp() {
+	elevio.SetMotorDirection(elevio.MD_Up)
+}
+
+func GoDown() {
+	elevio.SetMotorDirection(elevio.MD_Down)
+}
+
+func Stop() {
+	elevio.SetMotorDirection(elevio.MD_Stop)
+}
+
+func ServeOrder(currentFloor int, toFloor int) elevio.MotorDirection {
+	dir := get_elevator_dir(currentFloor, toFloor)
+	elevio.SetMotorDirection(dir)
+	return dir
+}
+
+func get_elevator_dir(floor int, toFloor int) elevio.MotorDirection {
+	if toFloor == floor {
+		return elevio.MD_Stop
+	} else if toFloor > floor {
+		return elevio.MD_Up
+	} else {
+		return elevio.MD_Down
+	}
+}
+
+func OpenDoors(doorsOpenCh chan bool, obstructionCh chan bool) {
+    fmt.Println("doors")
+    ticker := time.NewTicker(500*time.Millisecond)
+    counter := 0
+	elevio.SetDoorOpenLamp(true)
     for {
-        select {
-        case floor := <- floorSenseCh:
-            Stop()
-            return floor
-        case <- ticker.C:
-            if !hasTimedOut {
-                hasTimedOut = true
-                GoDown()
+        select{
+            case obstructed := <- obstructionCh:
+            if obstructed {
+                ticker.Stop()
             } else {
-                Stop()
-                panic("Elevator is stuck!")
+                ticker.Reset(500 * time.Millisecond)
+            }
+        case <- ticker.C:
+            fmt.Println("Ticker..")
+            counter += 1
+            if counter >= 6 {
+                elevio.SetDoorOpenLamp(false)
+                doorsOpenCh <- true
+                return
             }
         }
     }
 }
 
-func PollElevatorIO(buttonCh chan elevio.ButtonEvent, florrSensCh chan int, StopButtonCh chan bool) {
-    go elevio.PollButtons(buttonCh)
-    go elevio.PollFloorSensor(florrSensCh)
-    go elevio.PollStopButton(StopButtonCh)
-}
-
-
-func GoUp() {
-    elevio.SetMotorDirection(elevio.MD_Up)
-}
-
-func GoDown() {
-    elevio.SetMotorDirection(elevio.MD_Down)
-}
-
-func Stop() {
-    elevio.SetMotorDirection(elevio.MD_Stop)
-}
-
-func ServeOrder(currentFloor int, toFloor int) (elevio.MotorDirection) {
-    dir := get_elevator_dir(currentFloor, toFloor)     
-    elevio.SetMotorDirection(dir)
-    return dir
-}
-
-func get_elevator_dir(floor int, toFloor int) (elevio.MotorDirection) {
-    if toFloor == floor {
-        return elevio.MD_Stop
-    } else if toFloor > floor {
-        return elevio.MD_Up
-    } else {
-        return elevio.MD_Down
-    }
-}
-
-func OpenDoors(doorsOpenCh chan bool) {
-    fmt.Println("Opening doors")
-    elevio.SetDoorOpenLamp(true)
-    time.Sleep(1 * time.Second) // TODO: Spesification says 3 sec
-    elevio.SetDoorOpenLamp(false)
-    fmt.Println("Doors closed")
-    doorsOpenCh <- true
-    fmt.Println("Doors closed")
-    // TODO: open the doors
-}
-
 func SetLights(e Elevator) {
-    for f := e.MinFloor; f <= e.MaxFloor; f++ {
-        elevio.SetButtonLamp(elevio.BT_HallUp, f, e.Requests.Up[f])
-        elevio.SetButtonLamp(elevio.BT_HallDown, f, e.Requests.Down[f])
-        elevio.SetButtonLamp(elevio.BT_Cab, f, e.Requests.ToFloor[f])
-    }
+	for f := e.MinFloor; f <= e.MaxFloor; f++ {
+		elevio.SetButtonLamp(elevio.BT_HallUp, f, e.Requests.Up[f])
+		elevio.SetButtonLamp(elevio.BT_HallDown, f, e.Requests.Down[f])
+		elevio.SetButtonLamp(elevio.BT_Cab, f, e.Requests.ToFloor[f])
+	}
 }
 
 func PrintElevator(e Elevator) {
-    fmt.Printf("Current floor: %d\n", e.CurrentFloor)
-    printState(e)
-    printDir(e.Dir)
-    printRequests(e)
+	fmt.Printf("Current floor: %d\n", e.CurrentFloor)
+	printState(e)
+	printDir(e.Dir)
+	printRequests(e)
 }
 
-
 func printRequests(e Elevator) {
-    up := make([]string, 0)
-    down := make([]string, 0)
-    to_floor := make([]string, 0)
-    for f := e.MinFloor; f <= e.MaxFloor; f++ {
-        if e.Requests.Up[f] {
-            up = append(up, fmt.Sprintf("%d", f))
-        }
-        if e.Requests.Down[f] {
-            down = append(down, fmt.Sprintf("%d", f))
-        }
-        if e.Requests.ToFloor[f] {
-            to_floor = append(to_floor, fmt.Sprintf("%d", f))
-        }
-    }
-    fmt.Println("Requests:")
-    fmt.Printf("\tUp: %s\n", strings.Join(up, ","))
-    fmt.Printf("\tDown: %s\n", strings.Join(down, ","))
-    fmt.Printf("\tToFloor: %s\n", strings.Join(to_floor, ","))
+	up := make([]string, 0)
+	down := make([]string, 0)
+	to_floor := make([]string, 0)
+	for f := e.MinFloor; f <= e.MaxFloor; f++ {
+		if e.Requests.Up[f] {
+			up = append(up, fmt.Sprintf("%d", f))
+		}
+		if e.Requests.Down[f] {
+			down = append(down, fmt.Sprintf("%d", f))
+		}
+		if e.Requests.ToFloor[f] {
+			to_floor = append(to_floor, fmt.Sprintf("%d", f))
+		}
+	}
+	fmt.Println("Requests:")
+	fmt.Printf("\tUp: %s\n", strings.Join(up, ","))
+	fmt.Printf("\tDown: %s\n", strings.Join(down, ","))
+	fmt.Printf("\tToFloor: %s\n", strings.Join(to_floor, ","))
 }
 
 func printState(e Elevator) {
-    switch e.State {
-    case IDLE:
-        fmt.Println("State: Idle")
-    case MOVING:
-        fmt.Println("State: Moving")
-    case DOOR_OPEN:
-        fmt.Println("State: Door open")
-    }
+	switch e.State {
+	case IDLE:
+		fmt.Println("State: Idle")
+	case MOVING:
+		fmt.Println("State: Moving")
+	case DOOR_OPEN:
+		fmt.Println("State: Door open")
+	}
 }
 
 func printDir(dir elevio.MotorDirection) {
-    switch dir {
-    case elevio.MD_Stop:
-        fmt.Println("Dir: Stop")
-    case elevio.MD_Up:
-        fmt.Println("Dir: Up")
-    case elevio.MD_Down:
-        fmt.Println("Dir: Down")
-    }
+	switch dir {
+	case elevio.MD_Stop:
+		fmt.Println("Dir: Stop")
+	case elevio.MD_Up:
+		fmt.Println("Dir: Up")
+	case elevio.MD_Down:
+		fmt.Println("Dir: Down")
+	}
 }
-
-
--- localelevator/elevatorcontroller/elevatorcontroller.go
+++ localelevator/elevatorcontroller/elevatorcontroller.go
@@ -5,11 +5,11 @@ import (
 	"localelevator/elevator"
 	"localelevator/requests"
 	"log"
-
 	"os"
 	"os/exec"
 	"sanntid/watchdog"
 	"time"
+    "fmt"
 )
 
 func ListenAndServe(
@@ -18,72 +18,70 @@ func ListenAndServe(
 	elevatorStuckCh chan struct{},
 	stopedAtFloor chan int,
 	orderChan chan elevator.Order,
-    elevatorUpdateCh chan elevator.Elevator,
-    printEnabled bool,
+	elevatorUpdateCh chan elevator.Elevator,
+	printEnabled bool,
 ) {
+    fmt.Println("ASDASDF")
 	buttonCh := make(chan elevio.ButtonEvent)
-	floorSens_Ch := make(chan int)
-	stopButton_Ch := make(chan bool)
-	onDoorsClosing_Ch := make(chan bool)
+	floorSensCh := make(chan int)
+	stopButtonCh := make(chan bool)
+	onDoorsClosingCh := make(chan bool)
+	obstructionCh := make(chan bool)
 
-	elev_floor_timeout_ch := make(chan bool)
-	// elev_door_timeout_ch := make(chan bool)
-
-	floorWatchdog := watchdog.New(time.Second*10,
-		elev_floor_timeout_ch,
-        elevatorStuckCh,
+	floorWatchdog := watchdog.New(time.Second*15,
+		make(chan bool),
+		elevatorStuckCh,
 		func() {
 			elevator.Stop()
-            log.Fatal("floor watchdog")
+			log.Fatal("floor watchdog")
 		})
 
-	doorWatchdog := watchdog.New(time.Second*5,
+	doorWatchdog := watchdog.New(time.Second*30,
 		make(chan bool),
-        elevatorStuckCh,
+		elevatorStuckCh,
 		func() {
 			elevator.Stop()
-            log.Fatal("Door watchdog")
+			log.Fatal("Door watchdog")
 		})
 
 	go watchdog.Start(floorWatchdog)
 	go watchdog.Start(doorWatchdog)
 
-
 	ticker := time.NewTicker(time.Millisecond * 500)
-	elevator.PollElevatorIO(buttonCh, floorSens_Ch, stopButton_Ch)
+	elevator.PollElevatorIO(buttonCh, floorSensCh, stopButtonCh, obstructionCh)
 	for {
 		select {
-        case req := <- requestUpdateCh:
-            e.Requests = req
-            handleRequestUpdate(&e, onDoorsClosing_Ch)
-            elevatorUpdateCh <- e
-		case event := <- buttonCh:
-			//requests.UpdateRequests(event, *e.Requests)
-			//handle_button_press(event, e, doorClosedCh)
-            orderChan <- elevator.Order{
-                Type: event.Button,
-                AtFloor: event.Floor,
-            }
-		case event := <-floorSens_Ch:
-			watchdog.Feed(floorWatchdog)
-            hasStopped := handleFloorArrival(event, &e, onDoorsClosing_Ch)
-            if hasStopped {
-                stopedAtFloor <- event
-            }
+		case req := <-requestUpdateCh:
+			e.Requests = req
+			handleRequestUpdate(&e, onDoorsClosingCh, obstructionCh)
+			elevatorUpdateCh <- e
 
-		case <-onDoorsClosing_Ch:
-            stopedAtFloor <- e.CurrentFloor
-            e.Requests = <- requestUpdateCh 
-			handleDoorsClosing(&e, onDoorsClosing_Ch)
-			elevator.PrintElevator(e)
-            elevatorUpdateCh <- e
+		case event := <-buttonCh:
+			orderChan <- elevator.Order{
+				Type:    event.Button,
+				AtFloor: event.Floor,
+			}
+
+		case event := <-floorSensCh:
+			watchdog.Feed(floorWatchdog)
+			hasStopped := handleFloorArrival(event, &e, onDoorsClosingCh, obstructionCh)
+			if hasStopped {
+				stopedAtFloor <- event
+			}
+
+		case <-onDoorsClosingCh:
+			stopedAtFloor <- e.CurrentFloor
+			e.Requests = <-requestUpdateCh
+			handleDoorsClosing(&e, onDoorsClosingCh, obstructionCh)
+			elevatorUpdateCh <- e
+
 		case <-ticker.C:
-            if printEnabled {
-                cmd := exec.Command("clear")
-                cmd.Stdout = os.Stdout
-                cmd.Run()
-                elevator.PrintElevator(e)
-            }
+			if printEnabled {
+				cmd := exec.Command("clear")
+				cmd.Stdout = os.Stdout
+				cmd.Run()
+				elevator.PrintElevator(e)
+			}
 			if e.State != elevator.MOVING {
 				watchdog.Feed(floorWatchdog)
 			}
@@ -94,45 +92,51 @@ func ListenAndServe(
 	}
 }
 
-
-func handleRequestUpdate(e *elevator.Elevator, onDoorClosingCh chan bool) {
-    // TODO: The requests recieved from the request handler are assumed to be true,
-    // so we should now light the buttons
-    switch e.State {
-    case elevator.IDLE:
-        e.Dir, e.State = requests.GetNewDirectionAndState(*e)
-        if e.State == elevator.DOOR_OPEN {
-            elevator.Stop()
-            go elevator.OpenDoors(onDoorClosingCh)
-        } else {
-            elevio.SetMotorDirection(e.Dir)
-        }
-        // Probably no need to do anything on the other states:
-        // MOVING => next moves are handled by handleFloorArrival
-        // Doors open => next moves are handled by the handleDoorsClosing
-    }
+func handleRequestUpdate(e *elevator.Elevator,
+	onDoorClosingCh chan bool,
+	obstructionCh chan bool) {
+	// TODO: The requests recieved from the request handler are assumed to be true,
+	// so we should now light the buttons
+	switch e.State {
+	case elevator.IDLE:
+		e.Dir, e.State = requests.GetNewDirectionAndState(*e)
+		if e.State == elevator.DOOR_OPEN {
+			elevator.Stop()
+			go elevator.OpenDoors(onDoorClosingCh, obstructionCh)
+		} else {
+			elevio.SetMotorDirection(e.Dir)
+		}
+		// Probably no need to do anything on the other states:
+		// MOVING => next moves are handled by handleFloorArrival
+		// Doors open => next moves are handled by the handleDoorsClosing
+	}
 }
 
-func handleDoorsClosing(e *elevator.Elevator, onDoorClosingCh chan bool) {
-    e.Dir, e.State = requests.GetNewDirectionAndState(*e)
-    elevio.SetMotorDirection(e.Dir)
-    if e.State == elevator.DOOR_OPEN {
-        go elevator.OpenDoors(onDoorClosingCh)
-    }
+func handleDoorsClosing(e *elevator.Elevator,
+	onDoorClosingCh chan bool,
+	obstructionCh chan bool) {
+	e.Dir, e.State = requests.GetNewDirectionAndState(*e)
+	elevio.SetMotorDirection(e.Dir)
+	if e.State == elevator.DOOR_OPEN {
+		go elevator.OpenDoors(onDoorClosingCh, obstructionCh)
+	}
 }
 
-func handleFloorArrival(floor int, e *elevator.Elevator, onDoorsClosingCh chan bool) bool {
+func handleFloorArrival(floor int,
+	e *elevator.Elevator,
+	onDoorsClosingCh chan bool,
+	obstructionCh chan bool) bool {
 	e.CurrentFloor = floor
 	if e.State == elevator.IDLE || e.State == elevator.DOOR_OPEN {
 		return false
 	}
-    e.Dir, e.State = requests.GetNewDirectionAndState(*e)
-    if e.State == elevator.DOOR_OPEN {
-        elevator.Stop()
-        go elevator.OpenDoors(onDoorsClosingCh) 
-        return true
-    } else {
-        elevio.SetMotorDirection(e.Dir)
-    }
-    return false
+	e.Dir, e.State = requests.GetNewDirectionAndState(*e)
+	if e.State == elevator.DOOR_OPEN {
+		elevator.Stop()
+		go elevator.OpenDoors(onDoorsClosingCh, obstructionCh)
+		return true
+	} else {
+		elevio.SetMotorDirection(e.Dir)
+	}
+	return false
 }
